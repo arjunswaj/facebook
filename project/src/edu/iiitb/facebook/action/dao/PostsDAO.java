6c72185f75266d2ec4b68012500ebd6c99335a5a
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-04-10 16:14:26 +0530

    Implementation of likes for Posts.

--- project/src/edu/iiitb/facebook/action/dao/PostsDAO.java
+++ project/src/edu/iiitb/facebook/action/dao/PostsDAO.java
@@ -3,12 +3,21 @@ package edu.iiitb.facebook.action.dao;
 import java.util.List;
 
 import edu.iiitb.facebook.action.model.NewsFeed;
+import edu.iiitb.facebook.action.model.User;
 
 public interface PostsDAO
 {
 	public List<NewsFeed> getNewsFeedsForUser(int userId);
 
 	public int updateStatusForUser(int userId, String status);
+	
+	public void likeAPost(int postId, int userId);
+	
+	public int updateLikersCount(int postId);
+	
+	public void unlikeAPost(int postId, int userId);
+	
+	public List<User> peopleWholikeThePost(int postId);
 
 	public int deletePost(String postId);
 
