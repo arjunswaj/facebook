43045300a7940e0690e14ce01cb1049f9d32ac2e
prkreddy <prk596@gmail.com>
2014-04-12 12:06:15 +0530

    temp fix on updating userid in session while signup.

--- project/src/edu/iiitb/facebook/action/login/SecondSignUpAction.java
+++ project/src/edu/iiitb/facebook/action/login/SecondSignUpAction.java
@@ -13,76 +13,91 @@ import edu.iiitb.facebook.action.dao.UserDAO;
 import edu.iiitb.facebook.action.dao.impl.UserDAOImpl;
 import edu.iiitb.facebook.action.model.User;
 
-public class SecondSignUpAction extends ActionSupport implements SessionAware {
+public class SecondSignUpAction extends ActionSupport implements SessionAware
+{
 
 	private String phone_number;
 	private String secret_question;
 	private String secret_answer;
 	String ret;
 
-	public String getPhone_number() {
+	public String getPhone_number()
+	{
 		return phone_number;
 	}
 
-	public void setPhone_number(String phone_number) {
+	public void setPhone_number(String phone_number)
+	{
 		this.phone_number = phone_number;
 	}
 
-	public String getSecret_question() {
+	public String getSecret_question()
+	{
 		return secret_question;
 	}
 
-	public void setSecret_question(String secret_question) {
+	public void setSecret_question(String secret_question)
+	{
 		this.secret_question = secret_question;
 	}
 
-	public String getSecret_answer() {
+	public String getSecret_answer()
+	{
 		return secret_answer;
 	}
 
-	public void setSecret_answer(String secret_answer) {
+	public void setSecret_answer(String secret_answer)
+	{
 		this.secret_answer = secret_answer;
 	}
 
 	private Map<String, Object> session;
 
-	public Map<String, Object> getSession() {
+	public Map<String, Object> getSession()
+	{
 		return session;
 	}
 
-	public void setSession(Map<String, Object> session) {
+	public void setSession(Map<String, Object> session)
+	{
 		this.session = session;
 	}
 
 	UserDAO dao = new UserDAOImpl();
 	User user = new User();
 
-	public String execute() {
+	public String execute()
+	{
 		user = (User) session.get("user");
-		session.remove("user");
+
 		System.out.println(getPhone_number());
 		user.setPhoneNumber(getPhone_number());
 		user.setSecretQuestion(getSecret_question());
 		user.setSecretAnswer(getSecret_answer());
-		
+
 		ret = dao.setUserwithoutphotos(user);
+		user = dao.getUserByUserEmail(user.getEmail());
 		session.put("user", user);
 
 		System.out.println(ret);
 		return ret;
 	}
 
-	public void validate() {
+	public void validate()
+	{
 
-		if (StringUtils.isEmpty(phone_number)) {
+		if (StringUtils.isEmpty(phone_number))
+		{
 			addFieldError(phone_number, "phone number cannot be empty");
 		}
 
-		if (StringUtils.isEmpty(secret_question)) {
+		if (StringUtils.isEmpty(secret_question))
+		{
 			addFieldError(secret_question, "secret question cannot be empty");
 		}
 
-		if (StringUtils.isEmpty(secret_answer)) {
+		if (StringUtils.isEmpty(secret_answer))
+		{
 			addFieldError(secret_answer, "secret answer cannot be empty");
 		}
 
